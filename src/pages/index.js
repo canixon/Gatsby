6755387c69070f9527db5f7039e423c2cb76570c
Christopher Nixon <christopherdahl.nixon@gmail.com>
2022-10-21 15:11:59 -0400

    more works

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import "../components/index.css"
+import { navigate } from "gatsby"
+
 const Index = () => {
 	return (
+	<header className='hero'>
 		<div>
-		<Link to="/about">About Page</Link>
-		<h1>My Landing Page</h1>
-		<p>This is my landing page</p>
+			<div className='heroInfo'>
+			<h1>Christopher Nixon</h1>
+			<p>Computer Science and Pentesting</p>
+			<button className='heroButton' onClick={()=>{navigate("/about")}}>How we can help</button>
+			</div>
 		</div>
-	)
+	</header>
+	);
 
 }
 export default Index

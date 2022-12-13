38d0464b088e7ade435f0d5a2502af987aadc455
Christopher Nixon <59971156+canixon@users.noreply.github.com>
2022-12-12 17:43:32 -0700

    stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Card from '../components/card'
+import Card from "../components/card"
+import "../components/index.css"
 const Index = () => {
 	return ( 
 	<Layout>
-	<div className='Cards'>
+	<div className="Cards">
 	<h2>How we can help</h2>
+	<div className="cardGroup">
 	<Card 
 	title="Web design"
-	text="Sub title"/>
+	text="Sub title" />
 	<Card
 	title="Photography"
-	text="photos and stuff"/>
-	</div>		
+	text="photos and stuff" />
+	<Card
+	title="Photography"
+	text="photos and stuff" />
+	</div>	
+	</div>	
 	</Layout>
 	)
 }
